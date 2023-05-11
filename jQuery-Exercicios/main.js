6dd33a9fab371be3becf109e0e63b36116fe45ae
Matheus Henrique <inv.mhenrique@gmail.com>
2023-05-11 15:43:22 -0300

    primeiro commit

---
+++ jQuery-Exercicios/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const nomeTarefa = $('#inserir-tarefa').val();
+        const tarefa = $('<li></li>');
+        $(`<li>${nomeTarefa}</li>`).appendTo(tarefa);
+        $(tarefa).appendTo('ul');
+        $('#inserir-tarefa').val('');
+    })
+})
